1f2f2c75f0756ffcd3e57828814b049a6989563a
lamphc <lamphc@live.com>
2021-12-14 09:33:49 +0800

    axios封装说明

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,25 +1,27 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import { Message } from 'element-ui'
 import store from '@/store'
-import { getToken } from '@/utils/auth'
 
 // create an axios instance
+// 1. 通过axios.create工厂函数创建请求实例
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // 后台请求地址（根据环境变量动态配置）
+  baseURL: process.env.VUE_APP_BASE_API // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  // timeout: 5000 // request timeout
 })
 
 // request interceptor
+// 请求拦截器=》发请求之前执行（请求没有发出）
 service.interceptors.request.use(
   config => {
     // do something before request is sent
-
+    // 作用：统一添加token
     if (store.getters.token) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers['X-Token'] = store.getters.token
     }
     return config
   },
@@ -31,6 +33,7 @@ service.interceptors.request.use(
 )
 
 // response interceptor
+// 响应拦截器=》请求响应了（页面还没有拿到数据）
 service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
@@ -43,35 +46,14 @@ service.interceptors.response.use(
    * You can also judge the status by HTTP Status Code
    */
   response => {
+    // 正常：状态码200进入
     const res = response.data
-
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
-      return res
-    }
+    // 说明：返回之后，页面才能获取res数据
+    // 作用：简化返回给页面的数据
+    return res
   },
   error => {
+    // 出错：状态码400 500进入
     console.log('err' + error) // for debug
     Message({
       message: error.message,
