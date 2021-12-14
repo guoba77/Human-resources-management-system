95d8fa68169cecff9f2468c57bcc14d2bcb01de7
lamphc <lamphc@live.com>
2021-12-14 10:24:03 +0800

    样式说明

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,15 +2,22 @@
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
     <div class="test">修改test</div>
-    <p>git练习</p>
+    <p :style="{ color: stys.menuActiveText }">git练习</p>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+// 导入scss中导出的变量
+import stys from '@/styles/variables.scss'
+console.log('样式：', stys)
 export default {
   name: 'Dashboard',
+  data () {
+    return {
+      stys
+    }
+  },
   computed: {
     ...mapGetters([
       'name'
