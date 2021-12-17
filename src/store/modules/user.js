1a2e9080a5e76c9927054f326ae6655e27b9e310
lamphc <lamphc@live.com>
2021-12-17 09:02:14 +0800

    获取登录人信息vuex准备

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,17 +1,20 @@
 /**
  * 存储登录人信息
  */
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 // 导入持计划方法
 import * as auth from '@/utils/auth'
-console.log('导入auth模块所有方法：', auth)
+// console.log('导入auth模块所有方法：', auth)
+
 export default {
   // 开启命名空间（模块化）
   namespaced: true,
   // 定义数据变量（全局共享的）（和组件data类似的：私有）=》相同点：响应式的
   state: {
     // 默认从本地获取一次token（获取上一次存储的token）
-    token: auth.getToken() || ''
+    token: auth.getToken() || '',
+    // 当前登录人信息(结合后台返回数据的类型)
+    user: {}
   },
   // 修改state定义的变量（只能通过它修改）=》特点：同步
   mutations: {
@@ -32,6 +35,21 @@ export default {
     delToken (state) {
       state.token = ''
       auth.removeToken()
+    },
+    /**
+     *
+     * @param {*} state
+     * @param {*} user 调用setUser传入的用户信息数据
+     */
+    setUser (state, user) {
+      state.user = user
+    },
+    /**
+     * 清空登录人信息
+     * @param {*} state
+     */
+    delUser (state) {
+      state.user = {}
     }
   },
   // 获取后台数据的方法 =》特点：异步
@@ -48,6 +66,11 @@ export default {
       console.log(res)
       // 2. 调用mutations方法setToken存储
       commit('setToken', res)
+    },
+    async getUserAction ({ commit }) {
+      // 1. 获取登录人信息
+      const user = await getUserInfo()
+      console.log('登录人信息：', user)
     }
   }
 }
