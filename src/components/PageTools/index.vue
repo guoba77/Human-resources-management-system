c5fa339bf8eb32ed719d2fa89aa1449d46913b84
lamphc <lamphc@live.com>
2021-12-22 09:53:11 +0800

    通栏组件插槽优化

--- src/components/PageTools/index.vue
+++ src/components/PageTools/index.vue
@@ -4,13 +4,18 @@
     <div class="left">
       <div class="tips">
         <i class="el-icon-info" />
-        <span>本月: 社保在缴 公积金在缴</span>
+        <!-- <span>本月: 社保在缴 公积金在缴</span> -->
+        <!-- 使用具名插槽，动态接收传入的html结构 -->
+        <slot name="left"> 默认内容left </slot>
       </div>
     </div>
     <div class="right">
       <!-- 右侧 -->
-      <el-button type="primary" size="small">历史归档</el-button>
-      <el-button type="primary" size="small">导出</el-button>
+      <!-- <el-button type="primary" size="small">历史归档</el-button>
+      <el-button type="primary" size="small">导出</el-button> -->
+      <slot name="right">
+        <el-button type="primary" size="small">导出</el-button>
+      </slot>
     </div>
   </div>
 </template>
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -4,7 +4,17 @@
       <el-card shadow="always" :body-style="{ padding: '20px' }">
         <!-- 页面title -->
         <template #header>
-          <PageTools />
+          <!-- 通栏组件 -->
+          <PageTools>
+            <template #left>
+              <span>总计100条数据</span>
+            </template>
+            <template #right>
+              <el-button type="primary" size="small">导入excel</el-button>
+              <el-button type="primary" size="small">导出excel</el-button>
+              <el-button type="primary" size="small">新增员工</el-button>
+            </template>
+          </PageTools>
         </template>
         <!-- 页面的内容 -->
         <div>内容</div>
