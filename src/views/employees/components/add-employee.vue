30cec57f745bc2174cdeb7d6cc787c72afaa436a
lamphc <lamphc@live.com>
2021-12-22 17:06:13 +0800

    聘用形式选择数据项渲染

--- src/views/employees/components/add-employee.vue
+++ src/views/employees/components/add-employee.vue
@@ -2,38 +2,76 @@
   <!-- sync修饰可以同步修改数据 -->
   <el-dialog title="新增员工" :visible="showDialog" @close="close">
     <!-- 表单 -->
-    <el-form label-width="120px">
-      <el-form-item label="姓名">
-        <el-input style="width: 50%" placeholder="请输入姓名" />
+    <el-form ref="addForm" :model="formData" :rules="rules" label-width="120px">
+      <el-form-item label="姓名" prop="username">
+        <el-input
+          v-model="formData.username"
+          style="width: 50%"
+          placeholder="请输入姓名"
+        />
       </el-form-item>
-      <el-form-item label="手机">
-        <el-input style="width: 50%" placeholder="请输入手机号" />
+      <el-form-item label="手机" prop="mobile">
+        <el-input
+          v-model="formData.mobile"
+          style="width: 50%"
+          placeholder="请输入手机号"
+        />
       </el-form-item>
-      <el-form-item label="入职时间">
-        <el-date-picker style="width: 50%" placeholder="请选择入职时间" />
+      <el-form-item label="入职时间" prop="timeOfEntry">
+        <el-date-picker
+          v-model="formData.timeOfEntry"
+          style="width: 50%"
+          placeholder="请选择入职时间"
+        />
       </el-form-item>
-      <el-form-item label="聘用形式">
-        <!-- <el-select style="width: 50%" placeholder="请选择" /> -->
+      <el-form-item label="聘用形式" prop="formOfEmployment">
+        <el-select
+          v-model="formData.formOfEmployment"
+          style="width: 50%"
+          placeholder="请选择"
+        >
+          <!-- 下拉选项： label 下拉显示名  value 存储的值 -->
+          <el-option
+            v-for="item in employList"
+            :key="item.id"
+            :label="item.value"
+            :value="item.id"
+          ></el-option>
+        </el-select>
       </el-form-item>
-      <el-form-item label="工号">
-        <el-input style="width: 50%" placeholder="请输入工号" />
+      <el-form-item label="工号" prop="workNumber">
+        <el-input
+          v-model="formData.workNumber"
+          style="width: 50%"
+          placeholder="请输入工号"
+        />
       </el-form-item>
-      <el-form-item label="部门">
-        <el-input style="width: 50%" placeholder="请选择部门" />
+      <el-form-item label="部门" prop="departmentName">
+        <el-input
+          v-model="formData.departmentName"
+          style="width: 50%"
+          placeholder="请选择部门"
+        />
       </el-form-item>
-      <el-form-item label="转正时间">
-        <el-date-picker style="width: 50%" placeholder="请选择转正时间" />
+      <el-form-item label="转正时间" prop="correctionTime">
+        <el-date-picker
+          v-model="formData.correctionTime"
+          style="width: 50%"
+          placeholder="请选择转正时间"
+        />
       </el-form-item>
     </el-form>
     <!-- footer插槽 -->
     <template v-slot:footer>
       <el-button @click="$emit('update:showDialog')">取消</el-button>
-      <el-button type="primary">确定</el-button>
+      <el-button type="primary" @click="addEmploy">确定</el-button>
     </template>
   </el-dialog>
 </template>
 
 <script>
+import diction from '@/api/constant/employees'
+
 export default {
   props: {
     showDialog: {
@@ -41,7 +79,56 @@ export default {
       default: false
     }
   },
+  data () {
+    return {
+      // 聘用形式列表
+      employList: diction.hireType,
+      // 表单数据
+      formData: {
+        username: '', // 用户名
+        mobile: '', // 手机号
+        timeOfEntry: '', // 入职时间
+        formOfEmployment: '', // 聘用形式
+        workNumber: '', // 工号
+        departmentName: '', // 部门
+        correctionTime: '' // 转正时间
+      },
+      // 校验规则
+      rules: {
+        username: [
+          { required: true, message: '用户姓名不能为空', trigger: ['blur', 'change'] },
+          { min: 1, max: 4, message: '用户姓名为1-4位', trigger: ['blur', 'change'] }
+        ],
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: ['blur', 'change'] },
+          { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确', trigger: ['blur', 'change'] }
+        ],
+        formOfEmployment: [
+          { required: true, message: '聘用形式不能为空', trigger: ['blur', 'change'] }
+        ],
+        workNumber: [
+          { required: true, message: '工号不能为空', trigger: ['blur', 'change'] }
+        ],
+        departmentName: [
+          { required: true, message: '部门不能为空', trigger: ['blur', 'change'] }
+        ],
+        timeOfEntry: [
+          { required: true, message: '请选择入职时间', trigger: ['blur', 'change'] }
+        ]
+      }
+    }
+  },
   methods: {
+    // 提交：新增员工
+    async addEmploy () {
+      try {
+        await this.$refs.addForm.validate()
+        // 校验通过
+        console.log('校验通过')
+      } catch (error) {
+        console.log(error)
+      }
+    },
     // 关闭弹层执行
     close () {
       this.$emit('update:showDialog')
