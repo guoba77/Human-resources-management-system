ad9105d2ff06bfd86aa4d798dcf3581aac19b9b6
lamphc <lamphc@live.com>
2021-12-23 18:10:52 +0800

    更新个人详情

--- src/views/employees/components/user-info.vue
+++ src/views/employees/components/user-info.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="app-container">
     <!-- 个人信息 -->
-    <el-form label-width="100px">
-      <!-- 部门 -->
+    <el-form ref="fm" label-width="100px" :model="userDetail" :rules="rules">
+      <!--手机 聘用形式  -->
       <el-row class="inline-info">
         <el-col :span="12">
-          <el-form-item label="部门">
-            <el-input />
+          <el-form-item label="部门" prop="departmentName">
+            <el-input v-model="userDetail.departmentName" />
           </el-form-item>
         </el-col>
       </el-row>
-      <!-- 入职时间 -->
+      <!--手机 聘用形式  -->
       <el-row class="inline-info">
         <el-col :span="12">
-          <el-form-item label="入职时间">
+          <el-form-item label="手机" prop="mobile">
+            <el-input v-model="userDetail.mobile" disabled />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <!-- 工号 入职时间 -->
+      <el-row class="inline-info">
+        <el-col :span="12">
+          <el-form-item label="入职时间" prop="timeOfEntry">
             <el-date-picker
+              v-model="userDetail.timeOfEntry"
               type="date"
               class="inputW"
               value-format="yyyy-MM-dd"
@@ -27,31 +36,53 @@
         <el-col :span="12">
           <el-form-item label="员工头像">
             <!-- 放置上传图片 -->
-            <el-image :src="require('@/assets/common/head.jpg')"></el-image>
+            <el-image :src="userDetail.staffPhoto"></el-image>
           </el-form-item>
         </el-col>
       </el-row>
       <!-- 保存个人信息 -->
       <el-row class="inline-info" type="flex" justify="center">
         <el-col :span="12">
-          <el-button type="primary">保存更新</el-button>
+          <el-button type="primary" @click="hSaveUserDetailById">
+            保存更新
+          </el-button>
           <el-button @click="$router.back()">返回</el-button>
         </el-col>
       </el-row>
     </el-form>
   </div>
 </template>
-
 <script>
+import { saveUserDetailById } from '@/api/employees'
+
 export default {
   props: {
+    // 接收用户详情数据
     userDetail: {
       type: Object,
       default: () => ({})
     }
+  },
+  data () {
+    return {
+      rules: {
+        departmentName: [
+          { required: true, message: '部门不能为空', trigger: ['blur', 'change'] }
+        ],
+        timeOfEntry: [
+          { required: true, message: '请选择入职时间', trigger: ['blur', 'change'] }
+        ]
+      }
+    }
+  },
+  methods: {
+    hSaveUserDetailById () {
+      this.$refs.fm.validate(async flag => {
+        if (!flag) return
+        await saveUserDetailById(this.userDetail)
+        this.$message.success('更新成功')
+      })
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
