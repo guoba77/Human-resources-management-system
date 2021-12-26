c5af9c01e9770b2688091a839d317272a399b95a
lamphc <lamphc@live.com>
2021-12-26 10:29:41 +0800

    权限数据转化树形并渲染

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -9,13 +9,13 @@
           </el-row>
         </div>
         <!-- card body -->
-        <el-table border :data="list">
+        <el-table border :data="list" row-key="id">
           <el-table-column prop="name" label="名称" />
           <el-table-column prop="code" label="标识" />
           <el-table-column prop="description" label="描述" />
           <el-table-column label="操作">
-            <template>
-              <el-button type="text">添加</el-button>
+            <template #default="{ row }">
+              <el-button v-if="row.type === 1" type="text">添加</el-button>
               <el-button type="text">编辑</el-button>
               <el-button type="text">删除</el-button>
             </template>
@@ -28,10 +28,14 @@
 
 <script>
 import { getPermissionList } from '@/api/permisson'
+// 导入转换树形结构方法
+import { tranformTreeData } from '@/utils'
 export default {
   data () {
     return {
       // 权限点列表
+      // 权限点数据分类：1. 页面访问权限（父）  2. 页面下功能（按钮）操作权限（子）
+      // 后续做权限控制，也是基于以上两种分类权限数据做控制
       list: []
     }
   },
@@ -42,8 +46,8 @@ export default {
     //  获取权限点列表数据
     async getList () {
       const list = await getPermissionList()
-      console.table(list)
-      this.list = list
+      // console.table(list)
+      this.list = tranformTreeData(list)
     }
   }
 }
