42e7c4bc31fdb6de9f7d019d9d5b9c62763811f1
lamphc <lamphc@live.com>
2021-12-26 16:26:05 +0800

    获取角色列表

--- src/views/employees/components/assign-role.vue
+++ src/views/employees/components/assign-role.vue
@@ -5,15 +5,26 @@
     :visible="showRoleDialog"
     @close="close"
   >
-    <!-- 这里准备复选框 -->
+    <!-- 多选框：roleIds选中的角色ID的数组 -->
+    <el-checkbox-group v-model="roleIds">
+      <!-- 多选的选项 label 存值 -->
+      <el-checkbox v-for="role in roleList" :key="role.id" :label="role.id">{{
+        role.name
+      }}</el-checkbox>
+      <!-- <el-checkbox label="113">开发者</el-checkbox>
+      <el-checkbox label="115">人事</el-checkbox> -->
+    </el-checkbox-group>
     <template #footer>
       <el-button type="primary" size="small">确定</el-button>
-      <el-button size="small">取消</el-button>
+      <el-button size="small" @click="$emit('update:showRoleDialog')">
+        取消
+      </el-button>
     </template>
   </el-dialog>
 </template>
 
 <script>
+import { getRoleList } from '@/api/setting'
 export default {
   props: {
     showRoleDialog: {
@@ -21,7 +32,27 @@ export default {
       default: false
     }
   },
+  data () {
+    return {
+      // 角色列表
+      roleList: [],
+      // 选中的角色ID的数组
+      roleIds: []
+    }
+  },
+  created () {
+    this.getRoleList()
+  },
   methods: {
+    // 获取角色列表
+    async getRoleList () {
+      const { rows } = await getRoleList({
+        page: 1, // 页码
+        pagesize: 1000 // 每页多少条数据
+      })
+      console.log('角色列表：', rows)
+      this.roleList = rows
+    },
     // 通过子传父控制弹出层显示和隐藏
     close () {
       this.$emit('update:showRoleDialog')
