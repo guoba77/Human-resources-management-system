171414dfc419495453bda965d08712b1d37232b1
lamphc <lamphc@live.com>
2021-12-29 17:15:58 +0800

    雷达图按需引入

--- src/views/dashboard/components/chart.vue
+++ src/views/dashboard/components/chart.vue
@@ -6,7 +6,15 @@
 </template>
 
 <script>
-import * as echarts from 'echarts'
+// 全量引入
+// import * as echarts from 'echarts'
+// 按需引入
+import * as echarts from 'echarts/core'
+import { TooltipComponent } from 'echarts/components'
+import { RadarChart } from 'echarts/charts'
+import { CanvasRenderer } from 'echarts/renderers'
+echarts.use([TooltipComponent, RadarChart, CanvasRenderer])
+
 export default {
   mounted () {
     // 只能在这里初始化绘制
@@ -17,22 +25,41 @@ export default {
     initChart () {
       // 1. 基于准备好的dom，初始化echarts实例
       const myChart = echarts.init(this.$refs.chartBox)
-      // 2. 绘制图表 => 调用myChart.setOption(图表配置项对象)
-      myChart.setOption({
-        title: {
-          text: 'ECharts 入门示例'
-        },
+      // 2. 绘制图表 => 调用myChart.setOption(图表配置项对象option：不同图表配置)
+      // opt =》 代表不同图表的配置项
+      const opt = {
+        // 鼠标经过提示
         tooltip: {},
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        // 雷达图专用配置
+        radar: {
+          // 配置雷达图数据指标（维度）
+          indicator: [
+            { name: '工作效率', max: 100 },
+            { name: '考勤', max: 100 },
+            { name: '积极性', max: 100 },
+            { name: '帮助同事', max: 100 },
+            { name: '自主学习', max: 100 },
+            { name: '正确率', max: 100 }
+          ]
         },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
-      })
+        // 设置图表数据
+        series: [
+          {
+            type: 'radar',
+            data: [
+              {
+                value: [10, 1, 100, 5, 100, 0],
+                name: '张三'
+              },
+              {
+                value: [50, 50, 50, 50, 50, 10],
+                name: '李四'
+              }
+            ]
+          }
+        ]
+      }
+      myChart.setOption(opt)
     }
   }
 }
