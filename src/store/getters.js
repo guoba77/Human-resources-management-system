315f4cbd89a146098a532b697d06047d80d12287
lamphc <lamphc@live.com>
2021-12-29 15:14:50 +0800

    显示登录人信息

--- src/store/getters.js
+++ src/store/getters.js
@@ -8,6 +8,13 @@ const getters = {
   // 当前登录人头像
   avatar: state => state.user.user.staffPhoto,
   // 当前登录人用户名
-  name: state => state.user.user.username
+  name: state => state.user.user.username,
+  // 公司
+  company: state => state.user.user.company,
+  // 部门
+  dept: state => state.user.user.departmentName,
+  // 工号
+  workNumber: state => state.user.user.workNumber
+
 }
 export default getters
