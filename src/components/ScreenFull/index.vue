a14e7515c11e76834e4931d596164330ea124ca7
lamphc <lamphc@live.com>
2021-12-30 10:08:15 +0800

    实现全屏逻辑

--- src/components/ScreenFull/index.vue
+++ src/components/ScreenFull/index.vue
@@ -4,13 +4,34 @@
     <!-- svg-icon组件=》渲染svg图标
     用法：icon-class="svg图片名字"
     -->
-    <svg-icon icon-class="fullscreen" class="fullscreen" />
+    <svg-icon
+      icon-class="fullscreen"
+      class="fullscreen"
+      @click="toggleScreen"
+    />
   </div>
 </template>
 
 <script>
+import sf from 'screenfull'
 export default {
-  name: 'ScreenFull'
+  name: 'ScreenFull',
+  methods: {
+    // 开启/关闭全屏
+    toggleScreen () {
+      /**
+       * 1. 判断浏览器是否支持全屏功能
+       * 2. 如果支持调用toggle方法开启/关闭全屏
+       */
+      // sf.isEnabled 是false说明浏览器不支持
+      if (!sf.isEnabled) {
+        return this.$message.error('您的浏览器不支持全屏功能！')
+      }
+      // 支持就调用开启/关闭全屏
+      // 判断如果不是全屏，执行sf.toggle方法就开启全屏，相反关闭
+      sf.toggle()
+    }
+  }
 }
 </script>
 
