3968e0255ba882573b6072691b5d87ea986864a4
lamphc <lamphc@live.com>
2021-12-30 17:08:21 +0800

    扩展-cookie过期时间设置

--- src/components/Lang/index.vue
+++ src/components/Lang/index.vue
@@ -29,7 +29,18 @@ export default {
       this.$i18n.locale = lang // 设置给本地的i18n插件
       // 存储国际化切换的状态=》中文zh | 英文en
       ck.set('lang', lang)
+      this.setExpire()
       this.$message.success('切换多语言成功')
+    },
+    // 学习js-cookie设置过期时间
+    setExpire () {
+      /**
+       * 1. 默认过期时间是session会话形式，浏览器关闭就没了
+       * 2. 设置过期时间=》第三个参数传入：{expires:天数 | 日期对象}
+       */
+      // 3小时后过期
+      const ep = new Date(Date.now() + 3 * 60 * 60 * 1000)
+      ck.set('text141', 123, { expires: ep })
     }
   }
 }
